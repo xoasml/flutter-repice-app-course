8f761ebfe4f16f302e11a89f7260dc1463ef6a6e
TaeHoon <xoasml@gmail.com>
2026-01-12 14:21:31 +0900

    폰트 설정

---
+++ lib/ui/text_styles.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const TextStyle titleTextBold = TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headerTextBold = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle largeTextBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle normalTextBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle smallTextBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle smallerTextBold = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle titleTextRegular = TextStyle(
+    fontSize: 50,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle headerTextRegular = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle largeTextRegular = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle mediumTextRegular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle smallTextRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle smallerTextRegular = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.normal,
+  );
+}
