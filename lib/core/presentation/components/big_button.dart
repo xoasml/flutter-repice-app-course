a7efbc9303e3026bb0aef6f9452d966935f4af3d
TaeHoon <xoasml@gmail.com>
2026-01-12 16:01:21 +0900

    컴포넌트 작성 준비 및 폰트 설정

---
+++ lib/core/presentation/components/big_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BigButton extends StatelessWidget {
+  const BigButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
