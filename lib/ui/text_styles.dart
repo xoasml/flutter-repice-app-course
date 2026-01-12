73f052a09f60a4d2e23fcd06238d223cc16c03e6
TaeHoon <xoasml@gmail.com>
2026-01-12 15:39:12 +0900

    폰트 설정

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -4,60 +4,72 @@ abstract class TextStyles {
   static const TextStyle titleTextBold = TextStyle(
     fontSize: 50,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle headerTextBold = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle largeTextBold = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle normalTextBold = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle smallTextBold = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle smallerTextBold = TextStyle(
     fontSize: 11,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle titleTextRegular = TextStyle(
     fontSize: 50,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle headerTextRegular = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle largeTextRegular = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle mediumTextRegular = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle smallTextRegular = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
   static const TextStyle smallerTextRegular = TextStyle(
     fontSize: 11,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 }
